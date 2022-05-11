2b9b1f83c75a647160b08890e419b0d31683252c
Gustavo Henrique Ferreira Alves <42410863817@info.scs>
2022-05-11 11:52:02 -0200

    Fonte nova adicionada

--- ExpoAvanade/App.js
+++ ExpoAvanade/App.js
@@ -22,20 +22,9 @@ import {
   ABeeZee_400Regular,
   ABeeZee_400Regular_Italic
 } from '@expo-google-fonts/abeezee';
-import {
-  IBMPlexMono_100Thin,
-  IBMPlexMono_100Thin_Italic,
-  IBMPlexMono_300Light,
-  IBMPlexMono_300Light_Italic,
-  IBMPlexMono_400Regular,
-  IBMPlexMono_400Regular_Italic,
-  IBMPlexMono_500Medium,
-  IBMPlexMono_500Medium_Italic,
-  IBMPlexMono_600SemiBold,
-  IBMPlexMono_600SemiBold_Italic,
-  IBMPlexMono_700Bold,
-  IBMPlexMono_700Bold_Italic
-} from '@expo-google-fonts/ibm-plex-mono';
+import { 
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
 
 import { useFonts } from 'expo-font';
 import AppLoading from 'expo-app-loading';
@@ -44,24 +33,13 @@ export default function App() {
   let [fontsLoaded, error] = useFonts({
     ABeeZee_400Regular,
     ABeeZee_400Regular_Italic,
-    IBMPlexMono_100Thin,
-    IBMPlexMono_100Thin_Italic,
-    IBMPlexMono_300Light,
-    IBMPlexMono_300Light_Italic,
-    IBMPlexMono_400Regular,
-    IBMPlexMono_400Regular_Italic,
-    IBMPlexMono_500Medium,
-    IBMPlexMono_500Medium_Italic,
-    IBMPlexMono_600SemiBold,
-    IBMPlexMono_600SemiBold_Italic,
-    IBMPlexMono_700Bold,
-    IBMPlexMono_700Bold_Italic
+    Poppins_700Bold,
   });
 
   if (!fontsLoaded) {
     return <AppLoading />
   }
-  
+
   return (
     <NavigationContainer>
       <StatusBar
